6a95d706d1e67849a5bf3fac898a69b5abc7f2fe
davekirakosyan <dav.kyan@gmail.com>
2018-11-02 11:37:29 +0400

    Initial commit

---
+++ android/src/com/chemicalmagicians/liquidizer/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.chemicalmagicians.liquidizer;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.chemicalmagicians.liquidizer.Liquidizer;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Liquidizer(), config);
+	}
+}
